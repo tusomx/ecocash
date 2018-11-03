0fe536191720dcbc58ce0177a165fc57f94ca440
tusomx <tusomx@gmail.com>
2018-11-04 00:25:20 +0200

    Added support for transaction reversals

--- lib/ecocash/client.rb
+++ lib/ecocash/client.rb
@@ -67,6 +67,48 @@ module Ecocash
       JSON.parse(response.body)
     end
 
+    def transaction_reversal(msisdn, transaction_id, amount)
+      url = "#{Ecocash.configuration.api_base_url}/transactions/refund"
+      auth = {username: "#{Ecocash.configuration.username}", password: "#{Ecocash.configuration.password}"}
+       args = {
+          :clientCorrelator => "#{generated_client_correlator}",
+          :referenceCode => "#{Ecocash.configuration.reference_code}",
+          :tranType => "REF",
+          :endUserId => "#{msisdn}",
+          :originalEcocashReference => "#{transaction_id}",
+          :remark => "#{Ecocash.configuration.remarks}",
+          :paymentAmount => {
+             :charginginformation => {
+                :amount => "#{amount}",
+                :currency => "#{Ecocash.configuration.currency_code}",
+                :description => "#{Ecocash.configuration.description}"
+             },
+             :chargeMetaData => {
+               :channel => "SMS",
+                :purchaseCategoryCode => "#{Ecocash.configuration.purchase_category_code}",
+                :onBeHalfOf => "#{Ecocash.configuration.remarks}"
+             }
+          },
+          :merchantCode => "#{Ecocash.configuration.merchant_code}",
+          :merchantPin => "#{Ecocash.configuration.merchant_pin}",
+          :merchantNumber => "#{Ecocash.configuration.merchant_number}",
+          :currencyCode => "#{Ecocash.configuration.currency_code}",
+          :countryCode => "#{Ecocash.configuration.country_code}",
+          :terminalID => "#{Ecocash.configuration.terminal_id}",
+          :location => "#{Ecocash.configuration.location}",
+          :superMerchantName => "#{Ecocash.configuration.super_merchant_name}",
+          :merchantName => "#{Ecocash.configuration.merchant_name}"
+          }.to_json
+
+        options = {
+         :body => args,
+         :basic_auth => auth,
+         :headers => {'Content-Type' => 'application/json'}
+        }
+        response = self.class.post(url, options)
+        JSON.parse(response.body)
+    end
+
     private
 
     def generated_client_correlator
